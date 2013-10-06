f584b97972778de181b7054fd9c9273ddd12faaa
Antoine Blanchet <antoine.blanchet@invenietis.com>
2013-10-07 00:30:45 +0200

    Now 0.3.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Reflection;
 [assembly: AssemblyProduct( "Deployer" )]
 [assembly: AssemblyDescription( "Deployment helper command line application" )]
 
-[assembly: AssemblyVersion( "0.2.0.*" )]
-[assembly: AssemblyFileVersion( "0.2.0" )]
+[assembly: AssemblyVersion( "0.3.0.*" )]
+[assembly: AssemblyFileVersion( "0.3.0" )]
 [assembly: AssemblyConfiguration( "Debug" )]

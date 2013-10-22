63cd89262394c9a08f38257525fdd160a9dd7fd2
Antoine Blanchet <antoine.blanchet@invenietis.com>
2013-10-22 14:38:37 +0200

    fix bug when trying to restore a not existing backup file

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Reflection;
 [assembly: AssemblyProduct( "Deployer" )]
 [assembly: AssemblyDescription( "Deployment helper command line application" )]
 
-[assembly: AssemblyVersion( "0.4.0.*" )]
-[assembly: AssemblyFileVersion( "0.4.0" )]
+[assembly: AssemblyVersion( "0.4.1.*" )]
+[assembly: AssemblyFileVersion( "0.4.1" )]
 [assembly: AssemblyConfiguration( "Debug" )]
